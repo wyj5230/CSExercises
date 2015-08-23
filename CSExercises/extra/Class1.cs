851bec5543fa0026fe9ffc3f1918d2aac31b0b88
wyj5230 <810927890@qq.com>
2015-08-23 19:08:37 +0800

    case1

--- CSExercises/extra/Class1.cs
+++ CSExercises/extra/Class1.cs
@@ -12,25 +12,63 @@ namespace CSExercises
             string[] a = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
             string[] b = new string[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
             string[] hun = new string[] { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninty" };
+            {//thousand
+                if (Convert.ToInt32(input.Substring(0, 1)) == 0)
+                {
+                    if (Convert.ToInt32(input.Substring(1, 1)) >= 1)
+                        Console.Write(a[Convert.ToInt32(input.Substring(4, 1)) - 1] + " thousand");
+                }
+                else if (Convert.ToInt32(input.Substring(0, 1)) == 1)
+                {
+                    Console.Write(b[Convert.ToInt32(input.Substring(1, 1))] + " thousand");
+                }
+                else
+                {
+                    Console.Write(hun[Convert.ToInt32(input.Substring(0, 1)) - 2]);
+                    if (Convert.ToInt32(input.Substring(1, 1)) != 0)
+                        Console.Write(" "+a[Convert.ToInt32(input.Substring(1, 1)) - 1]);
+                    Console.Write(" thousand");
+                }
+            }
+            {//hundred
+                if (Convert.ToInt32(input.Substring(2, 1)) > 0)
+                    Console.Write(" " + a[Convert.ToInt32(input.Substring(2, 1)) - 1] + " hundred");
+            }
+            {//0.99
+                if (Convert.ToInt32(input.Substring(3, 1)) == 0)
+                {
+                    if (Convert.ToInt32(input.Substring(4, 1)) >= 1)
+                        Console.Write(" "+a[Convert.ToInt32(input.Substring(4, 1)) - 1] + " dollars");
+                }
+                else if (Convert.ToInt32(input.Substring(3, 1)) == 1)
+                {
+                    Console.Write(" " + b[Convert.ToInt32(input.Substring(4, 1))] + " dollars");
+                }
+                else
+                {
+                    Console.Write(" " + hun[Convert.ToInt32(input.Substring(3, 1)) - 2] + " ");
+                    if (Convert.ToInt32(input.Substring(4, 1)) != 0)
+                        Console.Write(a[Convert.ToInt32(input.Substring(4, 1)) - 1] + " dollars");
+                }
+            }
             {//cent
                 if (Convert.ToInt32(input.Substring(6, 1)) == 0)
                 {
-                    if (Convert.ToInt32(input.Substring(7, 1)) > 1)
-                        Console.Write(a[Convert.ToInt32(input.Substring(7, 1)) - 1] + " cent");
+                    if (Convert.ToInt32(input.Substring(7, 1)) >= 1)
+                        Console.Write( " and "+a[Convert.ToInt32(input.Substring(7, 1)) - 1] + " cents");
                 }
                 else if (Convert.ToInt32(input.Substring(6, 1)) == 1)
                 {
-                    Console.Write(b[Convert.ToInt32(input.Substring(7, 1))] + " cent");
-                }
-                else if (Convert.ToInt32(input.Substring(7, 1)) == 0)
-                {
-                    Console.Write(hun[Convert.ToInt32(input.Substring(6, 1)) - 2] + "cent");
-                }
+                    Console.Write(" and "+b[Convert.ToInt32(input.Substring(7, 1))] + " cents");
+                }             
                 else 
                 {
-                    Console.Write(hun[Convert.ToInt32(input.Substring(6, 1)) - 2] + " " + a[Convert.ToInt32(input.Substring(7, 1)) - 1]+" cent");
+                    Console.Write(" and " + hun[Convert.ToInt32(input.Substring(6, 1)) - 2] + " ");
+                        if (Convert.ToInt32(input.Substring(7, 1)) != 0)
+                        Console.Write(a[Convert.ToInt32(input.Substring(7, 1)) - 1]+" cents");
                 }
             }
+
         }
     }
 }
